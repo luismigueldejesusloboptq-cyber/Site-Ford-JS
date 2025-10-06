67284c961a5d4a95e0dec30081422c89496fa7b6
LUIS MIGUEL DE JESUS LOBO <021.909446@universidadesenaicimatec.edu.br>
2025-10-06 15:55:05 -0300

    carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,54 +1,48 @@
-
-
-//carousel
-
-
-//Array storage class
 let carouselArr = [];
 
-//class Carousel
+
 class Carousel {
-    constructor(image, title, link){
-        this.image = image
-        this.title = title 
-        this.link = link
-    }
+  constructor(image, title, link){
+    this.image = image;
+    this.title = title;
+    this.link = link;
+  }
+  
+  static Start(arr){
 
+    Carousel._carouselDiv = document.getElementById("carousel");
+    Carousel._carouselTitleDiv = document.getElementById("carousel-title");
     
+    if(arr && arr.length > 0){
+      Carousel._sequence = -1;
+      Carousel._size = arr.length;
       
-    static Start(arr){
-    const carouselDiv = document.getElementById("carousel")
-    const carouselTitleDiv = document.getElementById("carousel-title") 
-        
-        if(arr){
-
-            if(arr.length > 0){
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
-            }
-            
-        } else {
-            throw "Method Start need a Array Variable.";
-        }
+      Carousel.Next(); 
+      
+ 
+      Carousel._interval = setInterval(function(){ Carousel.Next(); }, 5000);
+    } else {
+      throw "O método Start precisa de um Array com itens.";
     }
+  }
 
-    static Next(){
-       if (!carouselDiv || !carouselTitleDiv) {
-        console.error("Elementos #carousel");
-        clearInterval(Carousel._interval);
-        return;
-       } 
-       Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
-
-       const currentItem = carouselArr[Carousel._sequence];
-       const imageHtml = <a href="${currentItem.link}">
-        <img src="${currentItem.image}" alt="${currentItem.title}"></img>
-       </a>;
-        const titleHtml = `<h2>${currentItem.title}</h2>`;
-
-        carouselDiv.innerHTML = imageHtml;
-        carouselTitleDiv.innerHTML = titleHtml;
-    }
-};
+  static Next(){
+    
+     if (!Carousel._carouselDiv || !Carousel._carouselTitleDiv) {
+      console.error("Elementos #carousel ou #carousel-title não encontrados no HTML.");
+      clearInterval(Carousel._interval);
+      return;
+    } 
+    Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
+    const currentItem = carouselArr[Carousel._sequence];
+    
+    const imageHtml = `<a href="${currentItem.link}">
+      <img src="img/${currentItem.image}" alt="${currentItem.title}">
+    </a>`;
+    
+    const titleHtml = `<h2>${currentItem.title}</h2>`;
+ 
+    Carousel._carouselDiv.innerHTML = imageHtml;
+   Carousel._carouselTitleDiv.innerHTML = titleHtml;
+ }
+}
